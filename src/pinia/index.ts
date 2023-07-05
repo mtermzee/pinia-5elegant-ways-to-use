53bc7e62960d4d6e2e2781f1df509423c11b85c7
Mohammad Termzee <mh.termzee@gitlab.com>
2023-07-05 16:09:49 +0200

    setup router and pinia to use them in stores

--- src/pinia/index.ts
+++ src/pinia/index.ts
@@ -1,3 +1,15 @@
 import { createPinia } from 'pinia';
+import { Router } from 'vue-router';
+import { router } from '@/router';
 
 export const pinia = createPinia();
+
+pinia.use(({ store })=>{
+    store.router = router;
+})
+
+declare module 'pinia' {
+    export interface PiniaCustomProperties {
+        router: Router;
+    }
+}
