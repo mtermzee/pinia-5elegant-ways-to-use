565db0d5c87c2edcd6f5441d12c65d443dac56a5
Mohammad Termzee <mh.termzee@gitlab.com>
2023-07-05 16:00:10 +0200

    split pinia service from main to make it global

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,8 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import { createPinia } from 'pinia';
+import { pinia } from "./pinia";
 
-const pinia = createPinia();
 const app = createApp(App);
 
 app.use(store).use(router).use(pinia).mount("#app");
